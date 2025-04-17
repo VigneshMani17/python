fdeb518d0143eab2e222686abb545e7c2a44d006
VigneshMani17 <mvignesh2747@gmail.com>
2025-04-17 17:26:52 +0530

    gemini api

---
+++ gemini_api.py
@@ -0,0 +1,29 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+# Create the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 2000,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro",
+  generation_config=generation_config,
+)
+
+chat_session = model.start_chat(
+  history=[
+  ]
+)
+
+response = chat_session.send_message("tell me about ipl in paragraph")
+
+print(response.text)
